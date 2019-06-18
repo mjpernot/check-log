4d08cb9a18af7dd95556f62e333ee4f3f952b4bd
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-06-18 09:35:29 +0100

    Update test/unit/check_log/open_log.py

--- test/unit/check_log/open_log.py
+++ test/unit/check_log/open_log.py
@@ -9,7 +9,6 @@
         test/unit/check_log/open_log.py
 
     Arguments:
-        None
 
 """
 
@@ -32,7 +31,6 @@ sys.path.append(os.getcwd())
 import check_log
 import version
 
-# Version
 __version__ = version.__version__
 
 
@@ -44,7 +42,7 @@ class UnitTest(unittest.TestCase):
 
     Super-Class:  unittest.TestCase
 
-    Sub-Classes:  None
+    Sub-Classes:
 
     Methods:
         setUp -> Unit testing initilization.
@@ -60,7 +58,6 @@ class UnitTest(unittest.TestCase):
         Description:  Initialization for unit testing.
 
         Arguments:
-            None
 
         """
 
@@ -75,8 +72,6 @@ class UnitTest(unittest.TestCase):
         Description:  Full check returns False.
 
         Arguments:
-            mock_marker -> Mock Ref:  check_log.find_marker
-            mock_chk -> Mock Ref:  check_log.full_chk
 
         """
 
@@ -94,8 +89,6 @@ class UnitTest(unittest.TestCase):
         Description:  Full check returns True.
 
         Arguments:
-            mock_open -> Mock Ref:  check_log.open
-            mock_chk -> Mock Ref:  check_log.full_chk
 
         """
 
