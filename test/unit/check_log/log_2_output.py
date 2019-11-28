add98e5acb53a0178ad4d71fd33fc59c6d79fb29
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-11-28 16:29:23 +0000

    Update test/unit/check_log/log_2_output.py

--- test/unit/check_log/log_2_output.py
+++ test/unit/check_log/log_2_output.py
@@ -42,10 +42,6 @@ class UnitTest(unittest.TestCase):
 
     Description:  Class which is a representation of a unit testing.
 
-    Super-Class:  unittest.TestCase
-
-    Sub-Classes:
-
     Methods:
         setUp -> Unit testing initilization.
         test_t_z_options_set -> Test with -t and -z options set.
