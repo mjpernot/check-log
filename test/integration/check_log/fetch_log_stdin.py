39d5c680ed7bcce1ece2f413e31be50e58d8cfd2
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-11-29 14:16:08 +0000

    Update test/integration/check_log/fetch_log_stdin.py

--- test/integration/check_log/fetch_log_stdin.py
+++ test/integration/check_log/fetch_log_stdin.py
@@ -30,6 +30,7 @@ import mock
 # Local
 sys.path.append(os.getcwd())
 import check_log
+import lib.gen_class as gen_class
 import version
 
 __version__ = version.__version__
@@ -41,17 +42,11 @@ class UnitTest(unittest.TestCase):
 
     Description:  Class which is a representation of a unit testing.
 
-    Super-Class:  unittest.TestCase
-
-    Sub-Classes:
-
     Methods:
         setUp -> Integration testing initilization.
-        test_multiple_lines2 -> Passing in multiple lines, partial find.
         test_no_lines -> Passing in no lines.
         test_multiple_lines -> Passing in multiple lines.
-        test_single_line_no_find -> Single line from standard - no find.
-        test_single_line_found -> Single line from standard - found.
+        test_single_line -> Single line from standard in.
 
     """
 
@@ -65,34 +60,10 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        self.base_dir = "test/integration/check_log"
-        self.test_path = os.path.join(os.getcwd(), self.base_dir, "testfiles")
-
-        self.file_marker = os.path.join(self.test_path,
-                                        "fetch_log_stdin_entry.txt")
-
-        self.args_array = {}
-
+        self.log = gen_class.LogFile()
         self.results0 = []
         self.results1 = ["Line one"]
         self.results2 = ["Line one", "Line two"]
-        self.results3 = ["Line two"]
-
-    @mock.patch("check_log.sys.stdin", io.StringIO(u"Line one\nLine two\n"))
-    def test_multiple_lines2(self):
-
-        """Function:  test_multiple_lines2
-
-        Description:  Passing in multiple lines, partial find.
-
-        Arguments:
-
-        """
-
-        self.args_array["-m"] = self.file_marker
-
-        self.assertEqual(check_log.fetch_log_stdin(self.args_array),
-                         self.results3)
 
     @mock.patch("check_log.sys.stdin", io.StringIO(u""))
     def test_no_lines(self):
@@ -105,8 +76,8 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        self.assertEqual(check_log.fetch_log_stdin(self.args_array),
-                         self.results0)
+        check_log.fetch_log_stdin(self.log)
+        self.assertEqual(self.log.loglist, self.results0)
 
     @mock.patch("check_log.sys.stdin", io.StringIO(u"Line one\nLine two\n"))
     def test_multiple_lines(self):
@@ -119,38 +90,22 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        self.assertEqual(check_log.fetch_log_stdin(self.args_array),
-                         self.results2)
+        check_log.fetch_log_stdin(self.log)
+        self.assertEqual(self.log.loglist, self.results2)
 
     @mock.patch("check_log.sys.stdin", io.StringIO(u"Line one\n"))
-    def test_single_line_no_find(self):
+    def test_single_line(self):
 
-        """Function:  test_single_line_no_find
+        """Function:  test_single_line
 
-        Description:  Single line from standard - no find.
+        Description:  Single line from standard in.
 
         Arguments:
 
         """
 
-        self.args_array["-m"] = self.file_marker
-
-        self.assertEqual(check_log.fetch_log_stdin(self.args_array),
-                         self.results0)
-
-    @mock.patch("check_log.sys.stdin", io.StringIO(u"Line one\n"))
-    def test_single_line_found(self):
-
-        """Function:  test_single_line_found
-
-        Description:  Single line from standard - found.
-
-        Arguments:
-
-        """
-
-        self.assertEqual(check_log.fetch_log_stdin(self.args_array),
-                         self.results1)
+        check_log.fetch_log_stdin(self.log)
+        self.assertEqual(self.log.loglist, self.results1)
 
 
 if __name__ == "__main__":
